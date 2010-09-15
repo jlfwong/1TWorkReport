b32cba61833822024e90b7c8ba7810a6a1cbce71
Jamie Wong <jamie.lf.wong@gmail.com>
2010-09-14 20:29:05 -0400

    Created rails projects

---
+++ factory_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FactoryBlog::Application.config.secret_token = '184acad1c8a1538f06ee350cee8d8d9f2e2eef0c6ee3e93e8a40374c99e6c93d36a46b570c3861a67d1221b39e6987818d7cbce52e479623230fb4763e29b727'
---
+++ fixture_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FixtureBlog::Application.config.secret_token = '48357e08e886a0ef33b7d543dac3b4fb59d24145b96aa3d9eb46772989deee5e0744aed889f7a72106490ae3324847304b9688585b6cb207ae75cf0320e6abb1'
